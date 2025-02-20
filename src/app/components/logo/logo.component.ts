3c73167d2bbb56c8eeb4e9ac466742ee6e0bb68f
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-02-20 17:42:35 +0000

    Added return to home click on logo

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -1,13 +1,25 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-logo',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.css',
   encapsulation: ViewEncapsulation.None
 })
 export class LogoComponent {
   @Input() width = 200;
+  @Input() useReturnToHome = false;
+
+  getLogoStyles() {
+    if (this.useReturnToHome) return "return-to-home-logo";
+    return "";
+  }
+
+  returnToHomeView() {
+    if (this.useReturnToHome) return "/";
+    return "";
+  }
 }
