193da3e901f63f5f048d89c31ed8460af3103d7c
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-02-21 13:03:06 +0000

    Changed icon stroke and size on login input

--- src/app/components/icons/close/close.component.ts
+++ src/app/components/icons/close/close.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   imports: [],
   template: `
     <svg [classList]="fillClass" width="50" height="50" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M52 2L2 52M2 2L52 52" stroke="#1E1E1E" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M52 2L2 52M2 2L52 52" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
   `,
   styles: ``,
