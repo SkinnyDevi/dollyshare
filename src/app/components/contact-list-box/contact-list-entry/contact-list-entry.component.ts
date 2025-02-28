e3fc7ecfe0013ab2bde4ce2bec3e8cf5562ca613
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-02-28 13:47:21 +0000

    Finish creating contact boxes and entries

--- src/app/components/contact-list-box/contact-list-entry/contact-list-entry.component.ts
+++ src/app/components/contact-list-box/contact-list-entry/contact-list-entry.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IconSelectorComponent } from "../../icons/icon-selector/icon-selector.component";
 
 @Component({
   selector: 'app-contact-list-entry',
   standalone: true,
-  imports: [],
+  imports: [IconSelectorComponent],
   templateUrl: './contact-list-entry.component.html',
   styleUrl: './contact-list-entry.component.css'
 })
 export class ContactListEntryComponent {
-
+  @Input() contact = "";
 }
--- src/app/views/user/manage-active-link/manage-active-link.component.ts
+++ src/app/views/user/manage-active-link/manage-active-link.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { IconButtonComponent } from "../../../components/app-button/icon-button/icon-button.component";
 import { ContactListBoxComponent } from "../../../components/contact-list-box/contact-list-box.component";
+import { AppButtonComponent } from "../../../components/app-button/app-button.component";
 
 @Component({
   selector: 'view-user-manage-active-link',
   standalone: true,
-  imports: [RouterLink, IconButtonComponent, ContactListBoxComponent],
+  imports: [RouterLink, IconButtonComponent, ContactListBoxComponent, AppButtonComponent],
   templateUrl: './manage-active-link.component.html',
   styleUrl: './manage-active-link.component.css'
 })
