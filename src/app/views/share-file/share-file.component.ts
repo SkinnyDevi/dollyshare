7d39e5865fe072bec330d02625c9f391adb3ef51
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-03-24 16:23:16 +0000

    Added file sizes on file upload

--- src/app/views/share-file/share-file.component.ts
+++ src/app/views/share-file/share-file.component.ts
@@ -46,6 +46,21 @@ export class ShareFileComponent {
     this.router.navigate(['/finish'])
   }
 
+  formatFileSize(bytes: number) {
+    if (bytes === 0) return '0 Bytes';
+
+    const units = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const factor = 1024;
+    let unitIndex = Math.floor(Math.log(bytes) / Math.log(factor));
+
+    unitIndex = Math.min(unitIndex, units.length - 1);
+
+    const size = bytes / Math.pow(factor, unitIndex);
+    const formattedSize = size % 1 === 0 ? size : size.toFixed(2);
+
+    return `${formattedSize} ${units[unitIndex]}`;
+  }
+
   getFileExtensionIcon(file: File) {
     const extension = file.name.substring(file.name.lastIndexOf('.') + 1);
 
